9f8ccd8a690c9f89dc51eae6ecaf93238c369afc
HyeonggyuKang <kin0204@naver.com>
2025-09-18 11:23:15 +0900

    ManyToOne Join Entity 예시
    section01/manytoone 예제;

---
+++ chap03-association-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/manytoone/ManyToOneAssociationTest.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.section01.manytoone;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+public class ManyToOneAssociationTest {
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        /* 설명. EntityManager가 생성될 때마다 고유의 새로운 영속성 컨텍스트(Entity 객체를 관리하는 창고)가 생성된다. */
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void 다대일_연관관계_객체_그래프_탐색을_이용한_조회_테스트(){
+        int menuCode = 15;
+
+        MenuAndCategory foundMenu = entityManager.find(MenuAndCategory.class, menuCode);
+        System.out.println("foundMenu = " + foundMenu);
+    }
+
+}
