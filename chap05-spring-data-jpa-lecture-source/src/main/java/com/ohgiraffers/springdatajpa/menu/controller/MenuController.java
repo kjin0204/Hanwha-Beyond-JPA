906aaa56591c3dc396f0302cf7f63296ce17b2f5
HyeonggyuKang <kin0204@naver.com>
2025-09-19 11:02:01 +0900

    key 값으로 단건 조회

--- chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/controller/MenuController.java
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/controller/MenuController.java
@@ -1,6 +1,8 @@
 package com.ohgiraffers.springdatajpa.menu.controller;
 
+import com.ohgiraffers.springdatajpa.menu.dto.MenuDTO;
 import com.ohgiraffers.springdatajpa.menu.service.MenuService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,26 +14,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/menu")
+@Slf4j // logger를 롬복으로 대체
 public class MenuController {
-    private final MenuService menuSerice;
+    private final MenuService menuService;
 
     /* 설명.
      *  Logger를 활용한 로그 생성
-     *
+     *  1. println보다 성능적으로 우수하다.
+     *  2. 외부 리소스 파일로 따로 저장 및 송출이 가능하다.
+     *  3. 로그레벨에 따른 확인 가능하다.(개발: debug, 서비스: info.....)
     * */
-    Logger logger =  LoggerFactory.getLogger(MenuController.class);
+//    Logger logger =  LoggerFactory.getLogger(MenuController.class);
+//    Logger logger =  LoggerFactory.getLogger(getClass()); // 현재 클래스의 메타정보를 넣어 줌
+
 
     @Autowired
-    public MenuController(MenuService menuSerice) {
-        this.menuSerice = menuSerice;
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
     }
 
     /* 설명. url 경로 상에 넘어온 값을 변수에 바로 담을 수 있다.(PathVariable이라고 한다.) */
     @GetMapping("/{menuCode}")
     public String findMenuByCode(@PathVariable int menuCode , Model model){
-        System.out.println("menuCode = " + menuCode);
-        logger.info("menuCode = {} " , menuCode);
+//        logger.info("menuCode = {} " , menuCode); Logger로 생성 했을때
+        log.debug("menuCode = {}" , menuCode); //롬복 Slf4j 어노테이션 사용
 
-        return null;
+        MenuDTO menu = menuService.findMenuByCode(menuCode);
+        model.addAttribute("menu", menu);
+
+        return "menu/detail";
     }
 }
--- chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
@@ -1,7 +1,34 @@
 package com.ohgiraffers.springdatajpa.menu.service;
 
+import com.ohgiraffers.springdatajpa.menu.dto.MenuDTO;
+import com.ohgiraffers.springdatajpa.menu.entity.Menu;
+import com.ohgiraffers.springdatajpa.menu.repository.MenuRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class MenuService {
+    private final MenuRepository menuRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public MenuService(MenuRepository menuRepository, ModelMapper modelMapper) {
+        this.menuRepository = menuRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public MenuDTO findMenuByCode(int menuCode) {
+        Menu menu = menuRepository.findById(menuCode)
+                .orElseThrow(IllegalArgumentException::new); // Optional로 반환을 하여 예외가 발생했을때 예외 객체를 반환
+        log.debug("service계층에서 하나의 메뉴 상세보기 : {}", menu);
+
+
+        return modelMapper.map(menu, MenuDTO.class); // Entity -> DTO로 스왑
+    }
 }
